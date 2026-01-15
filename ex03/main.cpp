40c2819722840d60bdb2fcd7104dd2a3cf950d53
Ana <anaigd93@gmail.com>
2026-01-15 11:51:41 +0100

    Minor changes

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -8,14 +8,18 @@ int main(){
     DiamondTrap diamondTrap1(nameCT);
     std::cout << "\n";
     std::cout << "*´¯`*..*´¯`* [ Display info: " << diamondTrap1.getName() << " ] *´¯`*..*´¯`* " << std::endl; 
-
     std::cout << "HitPoints: " << diamondTrap1.getHitPoints() << std::endl; 
     std::cout << "AtackDamage: " << diamondTrap1.getAttackDamage() << std::endl; 
     std::cout << "EnergyPoints: " << diamondTrap1.getEnergyPoints() << std::endl;
+    std::cout << std::endl;
     diamondTrap1.guardGate();
     diamondTrap1.highFivesGuys();
-    std::cout << "Name: diamondTrap1.getName() " << diamondTrap1.getName() << std::endl;
-    std::cout << "Name: diamondTrap1.ClapTrap::getName() " << diamondTrap1.ClapTrap::getName() << std::endl;
-
+    std::cout << std::endl;
+    std::cout << "NAME manually checked" << std::endl;
+    std::cout << "diamondTrap1.getName() " << diamondTrap1.getName() << std::endl;
+    std::cout << "diamondTrap1.ClapTrap::getName() " << diamondTrap1.ClapTrap::getName() << std::endl;
+    std::cout << std::endl;
+    std::cout << "NAME checked via whoAmI" << std::endl;
     diamondTrap1.whoAmI();
+    std::cout << std::endl;
 }
